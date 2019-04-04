8cc7095a610f678003b5daf113b1594b561bd305
ra754 <ra754@jla.rutgers.edu>
2019-04-04 13:32:44 -0400

    add bus icon drawable

--- app/src/main/java/com/example/busplaygroundkt/ui/MapFragment.kt
+++ app/src/main/java/com/example/busplaygroundkt/ui/MapFragment.kt
@@ -3,6 +3,8 @@ package com.example.busplaygroundkt.ui
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
@@ -17,11 +19,10 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.*
 import io.reactivex.Observable
 import com.google.android.gms.maps.model.PolylineOptions
-import com.google.android.gms.maps.model.Polyline
 import android.graphics.Color
-import com.example.busplaygroundkt.data.model.Stops
-import java.util.*
-import java.util.Map
+import com.example.busplaygroundkt.P
+import com.example.busplaygroundkt.ui.components.BusDrawable
+import kotlinx.coroutines.*
 
 
 class MapFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener{
@@ -72,7 +73,6 @@ class MapFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListe
 
     override fun onMapReady(map: GoogleMap?) {
         val nb = LatLng(Config.NJ_LAT, Config.NJ_LNG)
-        val markerList = mutableMapOf<String, Marker?>()
         val listOfCoordsForLine = mutableListOf<LatLng>()
         mMap = map
         mMap?.setOnMarkerClickListener(this)
@@ -80,98 +80,104 @@ class MapFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListe
 
         val routeid_id_2_location = mutableMapOf<String, LatLng>()
 
-        mMapViewModel.loadBusStops()?.observe(this, Observer { result ->
-            result?.forEach { response ->
-                Observable.just(response.data)
-                    .flatMapIterable { it }
-//                    .buffer(2)
-                    .subscribe { item ->
-                        if(routeid_id_2_location.get(item.stopID) == null){
-                            routeid_id_2_location.put(item.stopID, LatLng(item.location.lat,item.location.lng))
-                        }
-
-                    }
-            }
-
-        })
+//        mMapViewModel.loadBusStops()?.observe(this, Observer { result ->
+//            result?.forEach { response ->
+//                Observable.just(response.data)
+//                    .flatMapIterable { it }
+//                    .subscribe { item ->
+//                        if(routeid_id_2_location.get(item.stopID) == null){
+//                            routeid_id_2_location.put(item.stopID, LatLng(item.location.lat,item.location.lng))
+//                        }
+//
+//                    }
+//            }
+//
+//        })
         val colors = listOf(Color.BLACK,Color.BLUE,Color.RED,Color.MAGENTA)
-        val r  = Random()
 
-        val route = "Route LX"
+        val route = "Route EE"
         val options = PolylineOptions()
-        mMapViewModel.loadRoutes()?.observe(this, Observer { routes ->
-           routes
-               ?.filter { it.is_active }
-               ?.filter { it.long_name.equals(route)}
-               ?.forEach { route ->
-                   Observable.just(route.stops)
-                       .flatMapIterable { it }
-                       .buffer(2)
-                       .subscribe ({ item ->
-                                options.add(routeid_id_2_location.get(item.get(0)))
-                                    .add(routeid_id_2_location.get(item.get(1)))
-                                    .color(colors[3])
-                                    .width(9f)
-
-                                    }
-
-                           , {t: Throwable ->  t.printStackTrace()})
-
-               }
-
-            mMap?.addPolyline(options)
-
-        })
+//        mMapViewModel.loadRoutes()?.observe(this, Observer { routes ->
+//           routes
+//               ?.filter { it.is_active }
+//               ?.filter { it.long_name.equals(route)}
+//               ?.forEach { route ->
+//                   Observable.just(route.stops)
+//                       .flatMapIterable { it }
+//                       .buffer(2)
+//                       .subscribe ({ item ->
+//                                options.add(routeid_id_2_location.get(item.get(0)))
+//                                    .add(routeid_id_2_location.get(item.get(1)))
+//                                    .color(colors[3])
+//                                    .width(9f)
+//
+//                                    }
+//
+//                           , {t: Throwable ->  t.printStackTrace()})
+//
+//               }
+//
+//            mMap?.addPolyline(options)
+//
+//        })
 
         load_bus_into_ui(map)
         load_stop_into_ui(map)
         load_route_into_ui(map)
 
-//        mMapViewModel.loadBusData()?.observe(this, Observer { vehicles ->
-//            vehicles?.forEach { (routeid,bus) ->
-//                    if(markerList[routeid] != null){
-////                        println("updating bus position")
-//                        val marker = markerList[routeid]
-//                        marker?.position = LatLng(bus.lat, bus.lng)
-//                    } else {
-////                        println("creating bus cuz it didnt exists before")
-//
-//                        val marker= mMap?.addMarker(MarkerOptions().position(LatLng(bus.lat, bus.lng)).title(routeid))
-//                        markerList.put(routeid,marker)
-//                    }
-//            }
-//
-//        })
-//        val options = PolylineOptions()
+        mMapViewModel.loadBusData()?.observe(this, Observer { vehicles ->
+            vehicles?.forEach { (routeid,bus) ->
+                    if(markerList[routeid] != null){
+                        val marker = markerList[routeid]
+                        marker?.position = LatLng(bus.lat, bus.lng)
+                    } else {
+
+                        val bitmap = Bitmap.createBitmap(32,32,Bitmap.Config.ARGB_8888)
+                        val drawable  = resources.getDrawable(R.drawable.ic_bus_temp)
+                        val canvas = Canvas(bitmap)
+                        drawable.setBounds(0,0,bitmap.width,bitmap.height)
+                        drawable.draw(canvas)
+
+                        
+                        val marker= mMap?.addMarker(MarkerOptions().position(LatLng(bus.lat, bus.lng)).title(routeid))
+                        marker?.setIcon(BitmapDescriptorFactory.fromBitmap(bitmap))
+                        markerList.put(routeid,marker)
+                    }
+            }
+
+        })
 
     }
 
 
-//                response.data.forEach { stop ->
-//
-//                         println("${stop.location.lat},${stop.location.lng}")
-//
-//                          val line = map?.addPolyline(
-//                        PolylineOptions()
-//                            .add(LatLng(stop.location.lat, stop.location.lng), LatLng(40.7, -74.0))
-//                            .width(5f)
-//                            .color(Color.RED)
-//
-//
-//                         if(markerList[stop.stopID] != null){
-//                         val marker = markerList[stop.stopID]
-//                         marker?.position = LatLng(stop.location.lat, stop.location.lng)
-////                    }
-////                }
-//            }
-//        })
+    fun load_bus_into_ui(map: GoogleMap?){
+        val markerList = mutableMapOf<String, Marker?>()
 
-    fun load_bus_into_ui(map: GoogleMap?){}
-    fun load_route_into_ui(map: GoogleMap?){}
-    fun load_stop_into_ui(map: GoogleMap?){}
+    }
+    fun load_route_into_ui(map: GoogleMap?){
+
+    }
+    fun load_stop_into_ui(map: GoogleMap?){
+
+    }
 
     override fun onMarkerClick(p0: Marker?): Boolean {
         return false
+    }
+
+    private fun getDrawable(): BitmapDescriptor = runBlocking {
+         loadDrawable("BUS")
+    }
+
+    private suspend fun loadDrawable(busText: String): BitmapDescriptor {
+        val deferred_drawable : Deferred<BusDrawable> = GlobalScope.async {
+            delay(1000L)
+            BusDrawable(busText)
+        }
+        val final_drawable = deferred_drawable.await()
+        val final_icon: BitmapDescriptor = final_drawable.getMarkerIconFromDrawable()
+        return final_icon
+
     }
 
 
---
+++ app/src/main/java/com/example/busplaygroundkt/ui/components/BusDrawable.kt
@@ -0,0 +1,52 @@
+package com.example.busplaygroundkt.ui.components
+
+import android.graphics.*
+import android.graphics.drawable.Drawable
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+
+class BusDrawable(val busId: String) : Drawable() {
+
+    private val redPaint:Paint = Paint().apply { setARGB(255,255,0,0) }
+    private val someOtherPaint:Paint = Paint().apply { setARGB(255,9,220,0) }
+
+
+    // convert a drawable to a BitMapDescriptor for google maps marker
+
+
+    override fun draw(canvas: Canvas) {
+        val width: Int = bounds.width()
+        val height: Int = bounds.height()
+        val radius: Float = Math.min(width, height).toFloat() / 2f
+        canvas.drawCircle((width / 2).toFloat(), (height / 2).toFloat(), radius, redPaint)
+        canvas.drawText(busId, (width / 2).toFloat(), (height / 2).toFloat(), redPaint)
+
+    }
+
+    fun getMarkerIconFromDrawable() : BitmapDescriptor {
+        val canvas = Canvas()                                                                    // each pixel is 32 bit int , 4 bytes per bixel
+        val bitmap: Bitmap = Bitmap.createBitmap(this.intrinsicWidth,this.intrinsicHeight,Bitmap.Config.ARGB_8888)
+
+        canvas.setBitmap(bitmap)
+        this.apply {
+            setBounds(0,0,this.intrinsicWidth,this.intrinsicHeight)
+            draw(canvas)
+        }
+
+        return BitmapDescriptorFactory.fromBitmap(bitmap)
+    }
+
+
+
+    override fun setAlpha(alpha: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getOpacity(): Int = PixelFormat.OPAQUE
+
+    override fun setColorFilter(colorFilter: ColorFilter?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+}
